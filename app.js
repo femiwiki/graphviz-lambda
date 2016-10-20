d58ba9b3a451c34d9262ec7f82c0083edb37b988
suyooktang <suyook.tang@gmail.com>
2016-10-20 11:04:53 +0900

    .

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var ApiBuilder = require('claudia-api-builder'),
-  api = new ApiBuilder();
+      api = new ApiBuilder();
 module.exports = api;
 
 const rp = require('request-promise-native');
+const request = require("request");
 const child = require('child_process');
 const exec = require('child_process').exec;
 
-api.get('/svg', (req) =>
-    var url = req.queryString.url;
-    rp(url)
-      .then((graph) => child.spawnSync("./dot_static", ["-Tsvg"], {input: graph}).stdout.toString()),
-      {success: {contentType: 'image/svg+xml'}}
+api.get('svg', (request) =>
+  rp(request.queryString.url)
+    .then((graph) => child.spawnSync("./dot_static", ["-Tsvg"], {input: graph}).stdout.toString()),
+    {success: {contentType: 'image/svg+xml'}}
 );
 
